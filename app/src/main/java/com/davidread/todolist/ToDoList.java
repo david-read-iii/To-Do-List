f72076eeb91c48e8152810121047706ca10b6a80
David Read <49120229+david-read-iii@users.noreply.github.com>
2022-02-24 19:00:59 -0500

    Add write to-dos to Downloads directory function to ToDoList class
    ToDoList.java:
    - Define writeListToStream() helper method to write each String in mTaskList to the given FileOutputStream.
    - Modify saveToFile() to use writeListToStream().
    - Modify readFromFile() to be more readable and to actually close the BufferedReader.
    - Define downloadFile() that writes all to-dos to a File in the Downloads directory of the device's external storage.
    - Enhance documentation all around.

--- app/src/main/java/com/davidread/todolist/ToDoList.java
+++ app/src/main/java/com/davidread/todolist/ToDoList.java
@@ -1,8 +1,10 @@
 package com.davidread.todolist;
 
 import android.content.Context;
+import android.os.Environment;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -72,31 +74,80 @@ public class ToDoList {
     }
 
     /**
-     * Saves the to-dos stored in {@link #mTaskList} to a  file for persistent storage.
+     * Writes a {@link File} to the device's internal storage containing the to-dos stored in
+     * {@link #mTaskList}.
      */
     public void saveToFile() throws IOException {
         FileOutputStream outputStream = mContext.openFileOutput(FILENAME, Context.MODE_PRIVATE);
-        PrintWriter writer = new PrintWriter(outputStream);
-        for (String item : mTaskList) {
-            writer.println(item);
-        }
-        writer.close();
+        writeListToStream(outputStream);
     }
 
     /**
-     * Populates {@link #mTaskList} with the to-dos stored in the file.
+     * Populates {@link #mTaskList} with to-dos stored in a {@link File} written on the device's
+     * internal storage using {@link #saveToFile()}.
      */
     public void readFromFile() throws IOException {
-        FileInputStream inputStream = mContext.openFileInput(FILENAME);
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+
+        BufferedReader reader = null;
+
+        try {
+            FileInputStream inputStream = mContext.openFileInput(FILENAME);
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
             mTaskList.clear();
 
             String line;
             while ((line = reader.readLine()) != null) {
                 mTaskList.add(line);
             }
-        } catch (FileNotFoundException ex) {
+        } catch (FileNotFoundException e) {
             // Do nothing if FileNotFoundException.
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
         }
+    }
+
+    /**
+     * Writes a {@link File} to the device's external storage Downloads directory containing the
+     * to-dos stored in {@link #mTaskList}.
+     *
+     * @return Whether external storage is availiable to be written to.
+     */
+    public boolean downloadFile() throws IOException {
+
+        // Return with fail if the device's external storage is unavailable.
+        String state = Environment.getExternalStorageState();
+        if (!Environment.MEDIA_MOUNTED.equals(state)) {
+            return false;
+        }
+
+        // Get reference to the Downloads directory.
+        File downloadsDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+        // Create Downloads directory if it doesn't exist.
+        if (!downloadsDirectory.exists()) {
+            downloadsDirectory.mkdirs();
+        }
+
+        // Write file to Downloads directory.
+        File file = new File(downloadsDirectory, FILENAME);
+        writeListToStream(new FileOutputStream(file));
+
+        return true;
+    }
+
+    /**
+     * Writes each to-do stored in {@link #mTaskList} to the given {@link FileOutputStream}.
+     *
+     * @param outputStream {@link FileOutputStream} where the to-dos should be written.
+     */
+    private void writeListToStream(FileOutputStream outputStream) {
+        PrintWriter writer = new PrintWriter(outputStream);
+        for (String item : mTaskList) {
+            writer.println(item);
+        }
+        writer.close();
     }
 }
